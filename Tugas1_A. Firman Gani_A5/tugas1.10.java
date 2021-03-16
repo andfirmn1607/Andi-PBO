32e0b301f937ae00e0f9179c25f84352686bf8bb
andfirmn1607 <62065129+andfirmn1607@users.noreply.github.com>
2021-03-17 03:47:37 +0800

    Add files via upload

---
+++ Tugas1_A. Firman Gani_A5/tugas1.10.java
@@ -0,0 +1,9 @@
+class Hello {
+public static void main(String[] args) {
+System.out.print("Hello");
+/* menuliskan hello dan ganti baris*/
+System.out.print("\nHello ");
+/* menuliskan hello dan ganti baris*/
+System.out.println("World");
+System.out.println("Welcome");
+} }
